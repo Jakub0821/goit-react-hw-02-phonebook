32fd8c468f02ff92ad66c1c61c92f428590cd7c8
Jakub0821 <jakubmigda120@gmail.com>
2024-01-29 18:51:46 +0100

    change componets 02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
-import ContactForm from './ContactForm/ContactForm';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
+import ContactForm from './ContactForm/ContactForm.jsx';
+import ContactList from './ContactList/ContactList.jsx';
+import Filter from './Filter/Filter.jsx';
 
 export class App extends Component {
   state = {
